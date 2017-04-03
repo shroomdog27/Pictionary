79f12b6671bfdf24be04db3e3f4fdebec65e8e46
shroomdog27 <sweetiebelle@sweetiebelle.org>
2017-04-03 19:06:23 -0400

    Add bare bones main menu

--- src/main/java/org/sweetiebelle/pictionary/Pictionary.java
+++ src/main/java/org/sweetiebelle/pictionary/Pictionary.java
@@ -1,9 +1,35 @@
 package org.sweetiebelle.pictionary;
 
-public class Pictionary {
-    
-    
+import java.awt.EventQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+import org.sweetiebelle.pictionary.panels.MainMenu;
+import org.sweetiebelle.pictionary.panels.MainWindow;
+
+public class Pictionary implements Runnable {
+
+    private MainMenu menu;
+    private MainWindow window;
+
+    public Pictionary() {
+        window = new MainWindow();
+        try {
+            UIManager.setLookAndFeel(NimbusLookAndFeel.class.getName());
+        } catch (final Exception ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+        EventQueue.invokeLater(() -> run());
+    }
+
+    @Override
     public void run() {
-        // TODO: Program
+        window.setVisible(true);
+        //menu = new MainMenu();
+       // window.add(menu);
+       // menu.setVisible(true);
     }
 }
